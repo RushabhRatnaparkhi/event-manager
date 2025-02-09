a72c1e3220590c43cca561d674a5c479825224f1
RushabhRatnaparkhi <rushuratnaparkhi2@gmail.com>
2025-02-09 23:14:45 +0530

    ready for deployment

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
+// Use sessionStorage for better security
 api.interceptors.request.use((config) => {
   const token = sessionStorage.getItem('token');
   if (token) {
